bbf5aeb8002db45491b8a18219ec97860b4eb8b6
Annarex <alexkopica@mail.ru>
2022-05-03 00:29:07 +0300

    Добавлен выбор файла

--- app/src/main/java/com/donntu/teachjournal/MainActivity.kt
+++ app/src/main/java/com/donntu/teachjournal/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.donntu.teachjournal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     //удалил комментарий
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openFileDialog(view: View) {
+        val intent = Intent()
+            .setType("*/*")
+            .setAction(Intent.ACTION_GET_CONTENT)
+
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), 777)
     }
 }
