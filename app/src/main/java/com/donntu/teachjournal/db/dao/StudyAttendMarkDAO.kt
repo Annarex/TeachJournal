a78ebbb09d36c205043d9bba8e1407424c745974
Annarex <alexkopica@mail.ru>
2022-05-31 20:12:11 +0300

    Обновление запросов и дао

--- app/src/main/java/com/donntu/teachjournal/db/dao/StudyAttendMarkDAO.kt
+++ app/src/main/java/com/donntu/teachjournal/db/dao/StudyAttendMarkDAO.kt
@@ -4,6 +4,8 @@ import androidx.room.*
 import com.donntu.teachjournal.db.entity.AttendMarkType
 import com.donntu.teachjournal.db.entity.Journal
 import com.donntu.teachjournal.db.entity.StudyAttendMark
+import com.donntu.teachjournal.db.entity_with_relate.StudyAttendMarkWithInfo
+import com.donntu.teachjournal.db.entity_with_relate.StudyClassWithInfo
 import io.reactivex.Completable
 import io.reactivex.Maybe
 import java.io.Serializable
@@ -11,7 +13,10 @@ import java.io.Serializable
 @Dao
 interface StudyAttendMarkDAO {
     @Query("SELECT * FROM StudyAttendMark")
-    fun getStudyAttendMark(): List<StudyAttendMark>
+    fun getStudyAttendMark(): List<StudyAttendMarkWithInfo>
+
+    @Query("SELECT sc.id as id, * FROM StudyAttendMark as sam, StudyClass as sc where sc.id = sam.id_study_class and sc.id_journal=:id_journal and sam.id_student =:id_student")
+    fun getStudyAttendMarkByIdJournalAndIdStudent(id_journal:Long, id_student: Long): List<StudyAttendMarkWithInfo>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertStudyAttendMark(amp: StudyAttendMark):Long
--- app/src/main/java/com/donntu/teachjournal/db/dao/TaskAndTypeDAO.kt
+++ app/src/main/java/com/donntu/teachjournal/db/dao/TaskAndTypeDAO.kt
@@ -3,6 +3,7 @@ package com.donntu.teachjournal.db.dao
 import androidx.room.*
 import com.donntu.teachjournal.db.entity.Task
 import com.donntu.teachjournal.db.entity.TaskType
+import com.donntu.teachjournal.db.entity_with_relate.StudyTaskWithInfo
 import io.reactivex.Completable
 import io.reactivex.Maybe
 import java.io.Serializable
@@ -10,7 +11,10 @@ import java.io.Serializable
 @Dao
 interface TaskAndTypeDAO {
     @Query("SELECT * FROM Task")
-    fun getTask(): List<Task>
+    fun getTasks(): List<StudyTaskWithInfo>
+
+    @Query("SELECT * FROM Task where id_journal = :id ORDER by id_task_type, id_cur_num_task")
+    fun getTaskByIdJournal(id:Long): List<StudyTaskWithInfo>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertTask(amp: Task):Long
